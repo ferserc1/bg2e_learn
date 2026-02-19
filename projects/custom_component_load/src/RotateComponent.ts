01f21ba73335adc640640469caac03fca1c51c42
Fernando Serrano Carpena <ferserc1@gmail.com>
2026-02-19 13:01:33 +0100

    Se ha añadido el ejemplo custom_component_load

---
+++ projects/custom_component_load/src/RotateComponent.ts
@@ -0,0 +1,65 @@
+import Component from "bg2e-js/ts/scene/Component.ts";
+import Bg2KeyboardEvent, { SpecialKey } from "bg2e-js/ts/app/Bg2KeyboardEvent.ts";
+
+export default class RotateComponent extends Component {
+    private _animation: boolean = false;
+    private _speed: number = 1;
+
+    constructor() {
+        super("RotateComponent");
+    }
+
+    assign(other: Component): void {
+        if (other instanceof RotateComponent) {
+            this._animation = other._animation;
+            this._speed = other._speed;
+        }
+    }
+
+    clone(): Component {
+        const newComponent = new RotateComponent();
+        newComponent.assign(this);
+        return newComponent;
+    }
+
+    async deserialize(sceneData: any, _: any): Promise<void> {
+        if (sceneData.animation !== undefined && typeof sceneData.animation === "boolean") {
+            this._animation = sceneData.animation;
+        }
+        if (sceneData.speed !== undefined && typeof sceneData.speed === "number") {
+            this._speed = sceneData.speed;
+        }
+    }
+
+    // Getters y setters para animación y velocidad
+    get animation() {
+        return this._animation;
+    }
+
+    set animation(value: boolean) {
+        this._animation = value;
+    }
+
+    get speed() {
+        return this._speed;
+    }
+
+    set speed(value: number) {
+        this._speed = value;
+    }
+
+    willUpdate(delta: number) : void {
+        if (this.transform && this._animation) {
+            // Multiplicamos por la velocidad
+            this.transform.matrix.rotate(delta * 0.002 * this._speed, 0, 1, 0);
+            const numFrames = 10;
+            this.node!.postRedisplayFrames = this.node!.postRedisplayFrames < numFrames ? this.node!.postRedisplayFrames + 1 : numFrames;
+        }
+    }
+
+    keyUp(event: Bg2KeyboardEvent) : void {
+        if (event.key === SpecialKey.SPACE) {
+            this._animation = !this._animation;
+        }
+    }
+}
